a7b33de9d449b041b98ac246014267fe3c6c1809
irynaprofile <105556785+irynaprofile@users.noreply.github.com>
2022-11-17 20:10:30 -0600

    add takeoff

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function () {
+    const takeoff = document.getElementById("takeoff");
+    takeoff.addEventListener("click", function () {
+        let isReady = confirm('Confirm that the shuttle is ready for takeoff.');
+        if (!isReady) return;
+        
+        const flightStatus = document.getElementById("flightStatus");
+        flightStatus.innerText = 'Shuttle in flight.'
+
+    });
+});
